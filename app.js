ccd561818a87fccc55401d523a3d417ea1dc6260
David Park <dpark4657@gmail.com>
2018-07-22 23:34:00 -0700

    adds other basic types

--- app.js
+++ app.js
@@ -1,2 +1,107 @@
 "use strict";
-console.log('it works');
+// string
+var myName = 'David';
+// myName = 28;
+// number
+var myAge = 27;
+// myAge = 'David';
+// assign types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = '27';
+// array
+var hobbies = ['Gaming', 'Coding'];
+// hobbies = [1000]
+// tuples - Express a fixed length array where the elements don't need to be the same type. Order matters.
+var address = [2740, 'Mercer'];
+// enum - Gives friendly names to sets of numeric values. Benefit - less memory and typos.
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue;
+console.log(myColor); // 1
+// any - try not to use.
+var car = 'Honda';
+console.log(car);
+car = { brand: 'Honda' };
+console.log(car);
+// functions
+function returnMyName() {
+    return myName;
+}
+console.log(returnMyName());
+// void
+function sayHello() {
+    console.log('Hello!');
+}
+// argument types
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+// console.log(multiply(2, 'David'));
+console.log(multiply(2, 10));
+// function types
+var myMultiply;
+// myMultiply = sayHello;
+myMultiply = multiply;
+// objects
+var userData = {
+    name: 'David',
+    age: 27,
+};
+// userData = {}; The type is already inferred. Can't reassign.Properties must match.
+// explicit object type
+var userData2 = {
+    name: 'David',
+    age: 27,
+};
+// complex object
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types - Allowing more than 1 type
+var myRealRealAge = 27;
+myRealRealAge = '27';
+// myRealRealAge = false;
+// checking types
+var finalValue = 911;
+if (typeof finalValue === 'number') {
+    console.log('final value is a number');
+}
+// never - Does not return anything. Will rarely use. Possibly in a function where it shouldn't be reached.
+function neverReturns() {
+    throw new Error('An erorr');
+}
+// nullable types
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull; // undefined in JS and TS
+canAlsoBeNull = null; // undefined variables can be null
+var canThisBeAny = null; // any
+canThisBeAny = 12;
+// section 1 excercise
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: 'Max',
+    bankAccount: bankAccount,
+    hobbies: ['Sports', 'Cooking'],
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- app.ts
+++ app.ts
@@ -15,10 +15,10 @@ myRealAge = 27;
 let hobbies: string[] = ['Gaming', 'Coding'];
 // hobbies = [1000]
 
-// tuples - order matters
+// tuples - Express a fixed length array where the elements don't need to be the same type. Order matters.
 let address: [number, string] = [2740, 'Mercer'];
 
-// enum - less memory and typos
+// enum - Gives friendly names to sets of numeric values. Benefit - less memory and typos.
 enum Color {
   Gray,         // 0
   Green = 100,  // reassigned from 1
@@ -57,3 +57,79 @@ console.log(multiply(2, 10));
 let myMultiply: (val1: number, val2: number) => number;
 // myMultiply = sayHello;
 myMultiply = multiply;
+
+// objects
+let userData = {
+  name: 'David',
+  age: 27,
+};
+
+// userData = {}; The type is already inferred. Can't reassign.Properties must match.
+
+// explicit object type
+let userData2: { name: string, age: number } = {
+  name: 'David',
+  age: 27,
+}
+
+// complex object
+let complex: { data: number[], output: (all: boolean) => number[]} = {
+  data: [100, 3.99, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+}
+
+let complex2: Complex = {
+  data: [100, 3.99, 10],
+  output: function(all: boolean): number[] {
+    return this.data;
+  }
+}
+
+// type alias - Create a reusable type
+type Complex = { data: number[], output: (all: boolean) => number[] };
+
+// union types - Allowing more than 1 type
+let myRealRealAge: number | string = 27;
+myRealRealAge = '27';
+// myRealRealAge = false;
+
+// checking types
+let finalValue = 911;
+if (typeof finalValue === 'number') {
+  console.log('final value is a number');
+}
+
+// never - Does not return anything. Will rarely use. Possibly in a function where it shouldn't be reached.
+function neverReturns():never {
+  throw new Error('An erorr');
+}
+
+// nullable types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull; // undefined in JS and TS
+canAlsoBeNull = null; // undefined variables can be null
+let canThisBeAny = null; // any
+canThisBeAny = 12;
+
+// section 1 excercise
+
+type BankAccount = { money: number, deposit(value: number): void };
+
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value: number): void {
+    this.money += value;
+  }
+};
+
+let myself: { name: string, bankAccount: BankAccount, hobbies: string[] } = {
+  name: 'Max',
+  bankAccount: bankAccount,
+  hobbies: ['Sports', 'Cooking'],
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
